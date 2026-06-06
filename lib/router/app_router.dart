88d800891b2b0b99396a9fbb0f1290c4db864b8b
muhammad-thariq <mthariqd@gmail.com>
2026-06-06 20:41:32 +0700

    chore(app): change inital page

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -23,7 +23,7 @@ class AppRouter {
 
   static final GoRouter router = GoRouter(
     navigatorKey: _rootKey,
-    initialLocation: '/route',
+    initialLocation: '/onboarding',
     routes: [
       GoRoute(
         path: '/onboarding',
