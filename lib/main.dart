8badeb7076fe5cc1c6864ca6a86990be45d4b5cc
muhammad-thariq <mthariqd@gmail.com>
2026-06-06 23:06:04 +0700

    feat(app): add microsoft clarity configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:clarity_flutter/clarity_flutter.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 
 void main() {
-  runApp(const SiagaApp());
+  final config = ClarityConfig(
+    projectId: 'x26e73x16m',
+    // Use LogLevel.Verbose while testing to debug initialization issues.
+    logLevel: LogLevel.None,
+  );
+
+  runApp(
+    ClarityWidget(
+      app: const SiagaApp(),
+      clarityConfig: config,
+    ),
+  );
 }
